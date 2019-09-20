ca342ca6b91d7d3376718e1a0003bbaf69f2eec3
Hao Meiru <leonie.hao@gmail.com>
2019-09-20 15:38:13 +0800

    change query8

--- proj1/query/query8.sql
+++ proj1/query/query8.sql
@@ -1,10 +1,9 @@
--- find the name of author who wrote more books than articles
-SELECT A.name, COUNT(P.pubtype) FILTER ( WHERE P.pubtype = 'book' ) AS book_num,
-COUNT(P.pubtype) FILTER ( WHERE P.pubtype = 'article') AS article_num
+-- find the name of author who wrote more books than phdthesis 
+SELECT A.name
 FROM publication AS P, authored AS AP, author AS A
 WHERE (P.pubtype = 'book'
-  OR P.pubtype = 'article')
+  OR P.pubtype = 'phdthesis')
   AND P.pubid = AP.pubid
   AND AP.aid = A.aid
 GROUP BY A.name
-HAVING COUNT(P.pubtype) FILTER ( WHERE P.pubtype = 'book' ) > COUNT(P.pubtype) FILTER ( WHERE P.pubtype = 'article');
+HAVING COUNT(P.pubtype) FILTER ( WHERE P.pubtype = 'book' ) > COUNT(P.pubtype) FILTER ( WHERE P.pubtype = 'phdthesis');
